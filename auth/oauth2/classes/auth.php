fb1f425e6ea1af5d74c567ecd9fcc23c341e670f
evsoldatkin <e.v.soldatkin@gmail.com>
2021-03-12 20:17:18 +0300

    support rosnou oauth2 fix1

--- auth/oauth2/classes/auth.php
+++ auth/oauth2/classes/auth.php
@@ -521,16 +521,15 @@ class auth extends \auth_plugin_base {
         if (!$userwasmapped) {
             // No defined mapping - we need to see if there is an existing account with the same email.
 
+            $moodleuser = \core_user::get_user_by_email($userinfo['email']);
             //Core Fix Start
-            if (isset($userinfo['idnumber']->id) && $userinfo['idnumber']->id != '')
+            if (!$moodleuser && isset($userinfo['idnumber']->id) && $userinfo['idnumber']->id != '')
             {
                 global $DB;
                 $moodleuser = $DB->get_record('user', ['idnumber' => $userinfo['idnumber']->id]);
-                $issuer->set('requireconfirmation', false);
+                if ($moodleuser)
+                    $issuer->set('requireconfirmation', false);
             }
-            else
-                $moodleuser = \core_user::get_user_by_email($userinfo['email']);
-            //$moodleuser = \core_user::get_user_by_email($userinfo['email']);
             //Core Fix Finish
             if (!empty($moodleuser)) {
                 if ($issuer->get('requireconfirmation')) {
